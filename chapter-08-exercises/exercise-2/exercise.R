cd8559362a6bcff38972bbd1c70f898e6a366467
Sofyar Utomo <nyambik@Sofyars-MacBook-Pro.local>
2020-04-16 22:01:10 -0700

    Finished 7-1, 7-2, 8-1, partial 9-3

--- chapter-08-exercises/exercise-2/exercise.R
+++ chapter-08-exercises/exercise-2/exercise.R
@@ -2,32 +2,31 @@
 
 # Create a *list* of 10 random numbers. Use the `runif()` function to make a 
 # vector of random numbers, then use `as.list()` to convert that to a list
-
+ran_list <- as.list(runif(10))
 
 # Use `lapply()` to apply the `round()` function to each number, rounding it to 
 # the nearest 0.1 (one decimal place)
-
+lapply(ran_list, round, 1)
 
 # Create a variable 'sentence' that contains a sentence of text (something 
 # longish). Make the sentence lowercase; you can use a function to help.
-
+sentence <- tolower("I am in deep deprivation of sunlight and outdoor activities")
 
 # Use the `strsplit()` function to split the sentence into a vector of letters.
 # Hint: split on `""` to split every character
 # Note: this will return a _list_ with 1 element (which is the vector of letters)
-
+strsplit(sentence, split = "")
 
 # Extract the vector of letters from the resulting list
-
+letter_fix <- str_extract_all(strsplit(sentence, split = ""), "[a-z]+")
 
 # Use the `unique()` function to get a vector of unique letters
-
-
+unique(letter_fix, fromLast = TRUE)
+       
 # Define a function `count_occurrences` that takes in two parameters: a letter 
 # and a vector of letters. The function should return how many times that letter
 # occurs in the provided vector.
-# Hint: use a filter operation!
-
+# Hint: use a filter operation!}
 
 # Call your `count_occurrences()` function to see how many times the letter 'e'
 # is in your sentence.
